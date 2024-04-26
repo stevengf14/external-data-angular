0e7f42fc62a1a1f09298657d4d8b51dcc82975ba
Steven <stevengf14@gmail.com>
2024-04-25 23:05:39 -0500

    external data

--- src/app/lista-de-libros/lista-de-libros.component.ts
+++ src/app/lista-de-libros/lista-de-libros.component.ts
@@ -1,37 +1,33 @@
-import { Component, OnInit } from '@angular/core';
-import { Libro } from '../common/types';
-import { HttpClient} from '@angular/common/http';
+import { Component, OnInit } from "@angular/core";
+import { Libro } from "../common/types";
+import { HttpClient } from "@angular/common/http";
 
 @Component({
-  selector: 'app-lista-de-libros',
-  templateUrl: './lista-de-libros.component.html',
-  styleUrls: ['./lista-de-libros.component.css']
+  selector: "app-lista-de-libros",
+  templateUrl: "./lista-de-libros.component.html",
+  styleUrls: ["./lista-de-libros.component.css"],
 })
 export class ListaDeLibrosComponent implements OnInit {
-
   libros: any;
   errorHttp: boolean = false;
   cargando: boolean = false;
 
-
-  constructor(private http:HttpClient) {
-
-  }
+  constructor(private http: HttpClient) {}
 
   ngOnInit() {
     this.cargando = true;
     this.cargarLista();
   }
 
-
-  cargarLista(){
-
-    this.http.get('assets/json/lista-de-libros.json').subscribe(
-      (respuesta) => { this.libros = respuesta; this.cargando = false; } ,
-      (respuesta) => { this.errorHttp = true }
-    )
-
+  cargarLista() {
+    this.http.get("assets/json/lista-de-libros.json").subscribe(
+      (respuesta) => {
+        this.libros = respuesta;
+        this.cargando = false;
+      },
+      (respuesta) => {
+        this.errorHttp = true;
+      }
+    );
   }
-
-
 }
